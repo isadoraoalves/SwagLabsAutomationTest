0081b33fad297a16fc1e47f9f23c5e9a52724e3c
isadoraoalves <128647400+isadoraoalves@users.noreply.github.com>
2023-03-22 19:15:33 -0300

    Add files via upload

---
+++ SwagLabsAutomationTest/src/main/java/com/example/SwagLabsAutomationTest/SwagLabsAutomationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SwagLabsAutomationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagLabsAutomationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwagLabsAutomationTestApplication.class, args);
+	}
+
+}
---
+++ SwagLabsAutomationTest/src/test/java/com/example/SwagLabsAutomationTest/SwagLabsAutomationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SwagLabsAutomationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagLabsAutomationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
